422a759d95ffb7c367206a866b7ccaea50f2e668
floju20 <airbilly24929718@gmail.com>
2026-01-22 14:11:10 +0900

    Feat: [BOJ] Solved Problems

---
+++ PS/BOJ/01000-10000/02001-03000/2557.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+    string hello;
+    cout << "XXXX World! (X에 들어갈 말은?) ";
+    cin >> hello;
+
+    cout << hello << " World!" << endl;
+
+    return 0;
+}
